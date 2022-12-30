d2d59d525c8688d2a7bb0c4e40783729cba9e7a5
RadiantK <pps8853@gmail.com>
2022-12-30 18:03:27 +0900

    순환참조 문제를 없에기 위한 @JsonIgnore 설정
    API에서 JSON으로 타입변경이 일어날 때 순환 참조 문제를 없에기 위한 @JsonIgnore 설정

--- jpashop/src/main/java/com/jpabook/jpashop/domain/Delivery.java
+++ jpashop/src/main/java/com/jpabook/jpashop/domain/Delivery.java
@@ -1,5 +1,6 @@
 package com.jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.jpabook.jpashop.domain.item.DeliveryStatus;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,6 +17,7 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
+    @JsonIgnore
     @OneToOne(mappedBy = "delivery", fetch =  FetchType.LAZY)
     private Order order;
 
