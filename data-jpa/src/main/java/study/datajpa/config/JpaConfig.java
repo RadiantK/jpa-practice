987fe84547581f4e2ccc1a4b5a67f7509c26a4f6
RadiantK <pps8853@gmail.com>
2023-01-01 17:22:04 +0900

    JpaAuditing 사용을 위한 Config 파일 생성
    JpaAuditing을 설정 파일을 생성하고 Jpa관련 설정

---
+++ data-jpa/src/main/java/study/datajpa/config/JpaConfig.java
@@ -0,0 +1,19 @@
+package study.datajpa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.of(UUID.randomUUID().toString());
+    }
+}
